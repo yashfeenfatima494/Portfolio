d4a76a4c2f6e547e46bb11dc67fd4f1393413fa4
Yashfeen Fatima <yashfeen494@gmail.com>
2026-07-24 22:32:24 +0530

    formatted

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Blog = () => {
-    return (
-        <div className = "blogs">
-            <h1>This is Blogs page</h1>
-        </div>
-    )
+  return (
+    <div className="blogs">
+      <h1>This is Blogs page</h1>
+    </div>
+  )
 }
 
 export default Blog
